30827bf701799092f9badb6806638ef3d7ca2df3
Marko Kreen <markokr@gmail.com>
2010-11-24 09:37:36 +0200

    attregex: lost interpretation.dat, compile regex locally

--- test/attregex/run.sh
+++ test/attregex/run.sh
@@ -8,11 +8,12 @@ tests="$tests rightassoc.dat"
 #tests="$tests leftassoc.dat"
 tests="$tests forcedassoc.dat"
 tests="$tests repetition.dat"
+tests="$tests interpretation.dat"
 
 for t in $tests; do
-  #printf "%-16s" "$t"
+  printf "%-20s" "$t"
   #$rxtest < data/$t | grep -vE '(NOTE|Research)'
-  $rxtest < data/$t | tail -n +5 | grep -vE 'haskell|mimi'
+  $rxtest < data/$t | tail -n +4 | grep -vE 'haskell|mimi|NOTE'
 done
 
 #$rxtest < data/categorize.dat | tail -n +4
