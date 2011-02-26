6d4def477bcc17c9295af6dc45ecdf5136fd4100
Marko Kreen <markokr@gmail.com>
2011-02-26 13:13:08 +0200

    signal: compat sigevent struct

--- usual/signal.h
+++ usual/signal.h
@@ -32,6 +32,27 @@
 #include <signal.h>
 
 /*
+ * Compat sigevent
+ */
+
+#ifndef SIGEV_NONE
+#define SIGEV_NONE 0
+#define SIGEV_SIGNAL 1
+#define SIGEV_THREAD 2
+union sigval {
+	int sival_int;
+	void *sival_ptr;
+};
+struct sigevent {
+	int sigev_notify;
+	int sigev_signo;
+	union sigval sigev_value;
+	void (*sigev_notify_function)(union sigval);
+
+};
+#endif
+
+/*
  * Compat sigaction()
  */
 
