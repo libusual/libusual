4ceec106c3c3f97b1dd0fb0db1897aa69589d8a8
Marko Kreen <markokr@gmail.com>
2016-01-25 12:49:43 +0200

    tls: use SSL_MODE_RELEASE_BUFFERS
    This should use less memory for idle connections.

--- usual/tls/tls.c
+++ usual/tls/tls.c
@@ -302,6 +302,7 @@ tls_configure_ssl(struct tls *ctx)
 {
 	SSL_CTX_set_mode(ctx->ssl_ctx, SSL_MODE_ENABLE_PARTIAL_WRITE);
 	SSL_CTX_set_mode(ctx->ssl_ctx, SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER);
+	SSL_CTX_set_mode(ctx->ssl_ctx, SSL_MODE_RELEASE_BUFFERS);
 
 	SSL_CTX_set_options(ctx->ssl_ctx, SSL_OP_NO_SSLv2);
 	SSL_CTX_set_options(ctx->ssl_ctx, SSL_OP_NO_SSLv3);
