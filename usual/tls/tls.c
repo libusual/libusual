7c0ae09239b09853ac42cd0ea2f0c303f43de746
Marko Kreen <markokr@gmail.com>
2016-07-07 19:08:51 +0300

    tls: sync with EOF fix upstream

--- usual/tls/tls.c
+++ usual/tls/tls.c
@@ -521,12 +521,11 @@ tls_ssl_error(struct tls *ctx, SSL *ssl_conn, int ssl_ret, const char *prefix)
 		if ((err = ERR_peek_error()) != 0) {
 			errstr = ERR_error_string(err, NULL);
 		} else if (ssl_ret == 0) {
-			if ((ctx->state & TLS_HANDSHAKE_COMPLETE) == 0) {
-				errstr = "Unexpected EOF";
-			} else {
+			if ((ctx->state & TLS_HANDSHAKE_COMPLETE) != 0) {
 				ctx->state |= TLS_EOF_NO_CLOSE_NOTIFY;
 				return (0);
 			}
+			errstr = "unexpected EOF";
 		} else if (ssl_ret == -1) {
 			errstr = strerror(errno);
 		}
