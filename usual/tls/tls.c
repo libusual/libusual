1f03e100fa0ae305b1d8228e843245c07c21fec2
Marko Kreen <markokr@gmail.com>
2016-12-07 15:19:53 +0200

    tls: openssl 1.1 needs less setup

--- usual/tls/tls.c
+++ usual/tls/tls.c
@@ -40,11 +40,13 @@ tls_init(void)
 	if (tls_initialised)
 		return (0);
 
+#ifdef USE_LIBSSL_INTERNALS
 	SSL_load_error_strings();
 	SSL_library_init();
 
 	if (BIO_sock_init() != 1)
 		return (-1);
+#endif
 
 	if ((tls_config_default = tls_config_new()) == NULL)
 		return (-1);
@@ -63,14 +65,16 @@ tls_deinit(void)
 		tls_config_free(tls_config_default);
 		tls_config_default = NULL;
 
+#ifdef USE_LIBSSL_INTERNALS
 		EVP_cleanup();
 		CRYPTO_cleanup_all_ex_data();
 		BIO_sock_cleanup();
 		ERR_clear_error();
-#ifdef USE_LIBSSL_INTERNALS
 		ERR_remove_thread_state(NULL);
-#endif
 		ERR_free_strings();
+#else
+		OPENSSL_cleanup();
+#endif
 
 		tls_initialised = 0;
 	}
