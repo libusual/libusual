0ce9759d2887ad01e6c3c550f42650541651b692
Alexey Bashtanov <alexey@brandwatch.com>
2019-10-04 17:05:52 +0100

    fix-multline-log-messages

--- usual/logging.c
+++ usual/logging.c
@@ -168,7 +168,7 @@ void log_generic(enum LogLevel level, void *ctx, const char *fmt, ...)
 	/* replace '\n' in message with '\n\t', strip trailing whitespace */
 	if (strchr(msg, '\n')) {
 		char *dst = buf2;
-		for (; *msg && dst - buf < (int)sizeof(buf2) - 2; msg++) {
+		for (; *msg && dst - buf2 < (int)sizeof(buf2) - 2; msg++) {
 			*dst++ = *msg;
 			if (*msg == '\n')
 				*dst++ = '\t';
