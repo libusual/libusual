libusual/libusual

commit b1b2ebbdcb84bbd84d3b748528510023e36ba59c

Stop relying on TLS cipher settings from 2009 OpenBSD (#40)
- Change the keywords secure, normal and fast to mean default, as
  their concepts were 10 years out of date. This ensures old configs
  will migrate from (now) insecure configs to modern openssl secure
  defaults.
- Remove "legacy" and "insecure" keyword from TLS ciphers as these
  are meaningless and not stable over 15 years of staleness.
- Remove "legacy" keyword for DH that supported DH1024 which is not secure.
- For default, do not call SSL_CTX_set_cipher_list() so it uses
  the openssl defaults. In those systems with a system-wide
  crypto policy (Fedora, RHEL, CentOS and clones) this ensures
  conforming to those (eg "DEFAULT", "FUTURE", "FIPS", etc)
- For OCSP, no longer set some arcane TLS cipher with 3des, but use
  openssl/system defaults.
- Remove support for DHE 1024 for the TLS server, it is too weak.

usual/tls/tls.c
@@ -427,14 +427,25 @@ tls_configure_ssl(struct tls *ctx)
 	if ((ctx->config->protocols & TLS_PROTOCOL_TLSv1_3) == 0)
 		SSL_CTX_set_options(ctx->ssl_ctx, SSL_OP_NO_TLSv1_3);
 
+	/*
+	 * obsolete outdated keywords, turn them to default.
+	 * For default, don't call SSL_CTX_set_cipher_list()
+	 * so we inherit openssl's default, which can also
+	 * include a system-wide policy, such as on rhel/fedora
+	 * https://docs.fedoraproject.org/en-US/packaging-guidelines/CryptoPolicies/
+	 */
 	if (ctx->config->ciphers != NULL) {
-		if (SSL_CTX_set_cipher_list(ctx->ssl_ctx,
-		    ctx->config->ciphers) != 1) {
-			tls_set_errorx(ctx, "failed to set ciphers");
-			goto err;
+		if (strcasecmp(ctx->config->ciphers, "secure") != 0 &&
+		    strcasecmp(ctx->config->ciphers, "normal") != 0 &&
+		    strcasecmp(ctx->config->ciphers, "fast") != 0) {
+
+			if (SSL_CTX_set_cipher_list(ctx->ssl_ctx,
+				ctx->config->ciphers) != 1) {
+					tls_set_errorx(ctx, "failed to set ciphers");
+					goto err;
+			}
 		}
 	}
-
 	SSL_CTX_set_info_callback(ctx->ssl_ctx, tls_info_callback);
 
 #ifdef X509_V_FLAG_NO_CHECK_TIME

usual/tls/tls_compat.c
@@ -44,13 +44,6 @@
  * https://tools.ietf.org/html/draft-ietf-ipsec-skip-06
  */
 
-static const char file_dh1024[] =
-"-----BEGIN DH PARAMETERS-----\n"
-"MIGHAoGBAPSI/VhOSdvNILSd5JEHNmszbDgNRR0PfIizHHxbLY7288kjwEPwpVsY\n"
-"jY67VYy4XTjTNP18F1dDox0YbN4zISy1Kv884bEpQBgRjXyEpwpy1obEAxnIByl6\n"
-"ypUM2Zafq9AKUJsCRtMIPWakXUGfnHy9iUsiGSa6q6Jew1XpL3jHAgEC\n"
-"-----END DH PARAMETERS-----\n";
-
 static const char file_dh2048[] =
 "-----BEGIN DH PARAMETERS-----\n"
 "MIIBCAKCAQEA9kJXtwh/CBdyorrWqULzBej5UxE5T7bxbrlLOCDaAadWoxTpj0BV\n"
@@ -77,7 +70,7 @@ static const char file_dh4096[] =
 "-----END DH PARAMETERS-----\n";
 
 
-static DH *dh1024, *dh2048, *dh4096;
+static DH *dh2048, *dh4096;
 
 static DH *load_dh_buffer(struct tls *ctx, DH **dhp, const char *buf)
 {
@@ -109,23 +102,13 @@ static DH *dh_auto_cb(SSL *s, int is_export, int keylength)
 	bits = EVP_PKEY_bits(pk);
 	if (bits >= 3072)
 		return load_dh_buffer(ctx, &dh4096, file_dh4096);
-	if (bits >= 1536)
-		return load_dh_buffer(ctx, &dh2048, file_dh2048);
-	return load_dh_buffer(ctx, &dh1024, file_dh1024);
-}
-
-static DH *dh_legacy_cb(SSL *s, int is_export, int keylength)
-{
-	struct tls *ctx = SSL_get_app_data(s);
-	return load_dh_buffer(ctx, &dh1024, file_dh1024);
+	return load_dh_buffer(ctx, &dh2048, file_dh2048);
 }
 
 long SSL_CTX_set_dh_auto(SSL_CTX *ctx, int onoff)
 {
-	if (onoff == 0)
+	if (onoff == 0) {
 		return 1;
-	if (onoff == 2) {
-		SSL_CTX_set_tmp_dh_callback(ctx, dh_legacy_cb);
 	} else {
 		SSL_CTX_set_tmp_dh_callback(ctx, dh_auto_cb);
 	}
@@ -199,7 +182,6 @@ long SSL_CTX_set_ecdh_auto(SSL_CTX *ctx, int onoff)
 void tls_compat_cleanup(void)
 {
 #ifdef DH_CLEANUP
-	if (dh1024) { DH_free(dh1024); dh1024 = NULL; }
 	if (dh2048) { DH_free(dh2048); dh2048 = NULL; }
 	if (dh4096) { DH_free(dh4096); dh4096 = NULL; }
 #endif

usual/tls/tls_config.c
@@ -217,8 +217,7 @@ tls_config_parse_protocols(uint32_t *protocols, const char *protostr)
 			protos = TLS_PROTOCOLS_ALL;
 
 		proto = 0;
-		if (strcasecmp(p, "all") == 0 ||
-		    strcasecmp(p, "legacy") == 0)
+		if (strcasecmp(p, "all") == 0)
 			proto = TLS_PROTOCOLS_ALL;
 		else if (strcasecmp(p, "default") == 0 ||
 		    strcasecmp(p, "secure") == 0)
@@ -288,20 +287,25 @@ tls_config_set_ciphers(struct tls_config *config, const char *ciphers)
 {
 	SSL_CTX *ssl_ctx = NULL;
 
+	/*
+	 * obsolete outdated keywords, turn them to default.
+	 * For default, don't call SSL_CTX_set_cipher_list()
+	 * so we inherit openssl's default, which can also
+	 * include a system-wide policy, such as on rhel/fedora
+	 * https://docs.fedoraproject.org/en-US/packaging-guidelines/CryptoPolicies/
+	 */
 	if (ciphers == NULL ||
 	    strcasecmp(ciphers, "default") == 0 ||
-	    strcasecmp(ciphers, "secure") == 0)
-		ciphers = TLS_CIPHERS_DEFAULT;
-	else if (strcasecmp(ciphers, "compat") == 0 ||
-	    strcasecmp(ciphers, "legacy") == 0)
-		ciphers = TLS_CIPHERS_COMPAT;
-	else if (strcasecmp(ciphers, "insecure") == 0 ||
-	    strcasecmp(ciphers, "all") == 0)
-		ciphers = TLS_CIPHERS_ALL;
-	else if (strcasecmp(ciphers, "normal") == 0)
-		ciphers = TLS_CIPHERS_NORMAL;
-	else if (strcasecmp(ciphers, "fast") == 0)
-		ciphers = TLS_CIPHERS_FAST;
+	    strcasecmp(ciphers, "secure") == 0 ||
+	    strcasecmp(ciphers, "normal") == 0 ||
+	    strcasecmp(ciphers, "fast") == 0)
+	{
+		return set_string(&config->ciphers, ciphers);
+	}
+	/*
+	 * At this point, these are custom supplied openssl compatible
+	 * cipher strings.
+	 */
 
 	if ((ssl_ctx = SSL_CTX_new(SSLv23_method())) == NULL) {
 		tls_config_set_errorx(config, "out of memory");
@@ -329,8 +333,6 @@ tls_config_set_dheparams(struct tls_config *config, const char *params)
 		keylen = 0;
 	else if (strcasecmp(params, "auto") == 0)
 		keylen = -1;
-	else if (strcasecmp(params, "legacy") == 0)
-		keylen = 1024;
 	else {
 		tls_config_set_errorx(config, "invalid dhe param '%s'", params);
 		return (-1);

usual/tls/tls_internal.h
@@ -25,41 +25,6 @@
 
 #define _PATH_SSL_CA_FILE USUAL_TLS_CA_FILE
 
-/*
- * Anything that is not completely broken.
- *
- * Also fixes 3DES ordering bug in older OpenSSLs.
- */
-#define TLS_CIPHERS_COMPAT	"HIGH:+3DES:!aNULL"
-
-/*
- * All==insecure.
- */
-#define TLS_CIPHERS_ALL		"ALL:!aNULL:!eNULL"
-
-/*
- * TLSv1.2+AEAD+ECDHE/DHE.  CBC modes are dubious due to spec bugs in TLS.
- */
-#define TLS_CIPHERS_DEFAULT	"HIGH+EECDH:HIGH+EDH:!SSLv3:!SHA384:!SHA256:!DSS:!aNULL"
-
-/*
- * Compact subset of reasonable suites only.
- *
- * Priorities, in order:
- * - ECDHE > DHE > RSA
- * - AESGCM > CBC
- * - TLSv1.2 > TLSv1.0
- * - AES256 > AES128.
- */
-#define TLS_CIPHERS_NORMAL	"HIGH+EECDH:HIGH+EDH:HIGH+RSA:+SHA384:+SHA256:+SSLv3:+EDH:+RSA:-3DES:3DES+RSA:!CAMELLIA:!DSS:!aNULL"
-
-/*
- * Prefer performance if it does not affect security.
- *
- * Same as "normal" but prefers AES128 to AES256.
- */
-#define TLS_CIPHERS_FAST	"HIGH+EECDH:HIGH+EDH:HIGH+RSA:+AES256:+SHA256:+SHA384:+SSLv3:+EDH:+RSA:-3DES:3DES+RSA:!CAMELLIA:!DSS:!aNULL"
-
 union tls_addr {
 	struct in_addr ip4;
 	struct in6_addr ip6;

usual/tls/tls_ocsp.c
@@ -767,7 +767,6 @@ tls_ocsp_connection_setup(struct tls *ctx)
 
 		SSL_CTX_set_options(q->ssl_ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
 		SSL_CTX_clear_options(q->ssl_ctx, SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1 | SSL_OP_NO_TLSv1_2 | SSL_OP_NO_TLSv1_3);
-		SSL_CTX_set_cipher_list(q->ssl_ctx, TLS_CIPHERS_COMPAT);
 		SSL_CTX_set_mode(q->ssl_ctx, SSL_MODE_AUTO_RETRY);
 
 		q->bio = BIO_new_ssl_connect(q->ssl_ctx);

usual/tls/tls_server.c
@@ -77,8 +77,6 @@ tls_configure_server(struct tls *ctx)
 
 	if (ctx->config->dheparams == -1)
 		SSL_CTX_set_dh_auto(ctx->ssl_ctx, 1);
-	else if (ctx->config->dheparams == 1024)
-		SSL_CTX_set_dh_auto(ctx->ssl_ctx, 2);
 
 	if (ctx->config->ecdhecurve == -1) {
 		SSL_CTX_set_ecdh_auto(ctx->ssl_ctx, 1);

commit 082096a6379f845f08cbd69e0d502c1085edefef

Fix bug caused by #40
The change to use default ciphers accidentally broke usage of the
the string "default" as the cipherlist. This fixes that.

usual/tls/tls.c
@@ -435,7 +435,8 @@ tls_configure_ssl(struct tls *ctx)
 	 * https://docs.fedoraproject.org/en-US/packaging-guidelines/CryptoPolicies/
 	 */
 	if (ctx->config->ciphers != NULL) {
-		if (strcasecmp(ctx->config->ciphers, "secure") != 0 &&
+		if (strcasecmp(ctx->config->ciphers, "default") != 0 &&
+		    strcasecmp(ctx->config->ciphers, "secure") != 0 &&
 		    strcasecmp(ctx->config->ciphers, "normal") != 0 &&
 		    strcasecmp(ctx->config->ciphers, "fast") != 0) {
 