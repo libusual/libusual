577db510e51f0b4f4622292091d1890cd22af0ba
GuanqunYang193 <144971563+GuanqunYang193@users.noreply.github.com>
2025-02-23 06:32:43 -0500

    Fix unit test and CI (#65)
    This PR fixs tls unit test and CI.
    The CI is broken because:
    1. FreeBSD image not found[1]
    2. tls test on Linux (Red Hat) container:rockylinux:9 not correct[2]
    
    [1]: https://github.com/libusual/libusual/runs/37664000964
    [2]: https://github.com/libusual/libusual/pull/64/checks?check_run_id=37664121141

--- test/test_common.h
+++ test/test_common.h
@@ -4,6 +4,7 @@
 #include "tinytest_macros.h"
 
 #define str_check(a, b) tt_str_op(a, ==, b)
+#define str_contains_check(a, b) tt_str_contains_op(a, "has", b)
 #define int_check(a, b) tt_int_op(a, ==, b)
 #define ull_check(a, b) tt_assert_op_type(a, ==, b, uint64_t, "%" PRIu64)
 
