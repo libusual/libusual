e1442f89a4717ce7915d56642a31b11f055c923e
Beihao Zhou <beihaozhou@gmail.com>
2025-03-02 15:50:56 -0500

    use macro for const

--- usual/caslock.c
+++ usual/caslock.c
@@ -1,27 +1,31 @@
 #include <usual/caslock.h>
 #include <usual/logging.h>
 
+#define CAS_LOCK_UNLOCKED 0
+#define CAS_LOCK_LOCKED 1
+#define CAS_LOCK_UNINITIALIZED 0
+#define CAS_LOCK_INITIALIZED 1
 
 void cas_lock_init(CasLock *lock) {
 #ifdef WIN32
-    InterlockedExchange(&lock->lock, 0);
+    InterlockedExchange(&lock->lock, CAS_LOCK_UNLOCKED);
 #else
-    lock->lock = 0;
+    lock->lock = CAS_LOCK_UNLOCKED;
 #endif
     lock->owner = 0;
-    lock->initialized = 1;
+    lock->initialized = CAS_LOCK_INITIALIZED;
 }
 
 void cas_lock_acquire(CasLock *lock) {
-    if (lock->initialized != 1) 
+    if (lock->initialized != CAS_LOCK_INITIALIZED) 
         fatal("Attempt to acquire an uninitialized lock!");
 
 #ifdef WIN32
-    while (InterlockedCompareExchange(&lock->lock, 1, 0) != 0) {
+    while (InterlockedCompareExchange(&lock->lock, CAS_LOCK_LOCKED, CAS_LOCK_UNLOCKED) != 0) {
         SwitchToThread();
     }
 #else
-    while (!__sync_bool_compare_and_swap(&lock->lock, 0, 1)) {
+    while (!__sync_bool_compare_and_swap(&lock->lock, CAS_LOCK_UNLOCKED, CAS_LOCK_LOCKED)) {
         sched_yield();
     }
 #endif
@@ -29,7 +33,7 @@ void cas_lock_acquire(CasLock *lock) {
 }
 
 void cas_lock_release(CasLock *lock) {
-    if (lock->initialized != 1) 
+    if (lock->initialized != CAS_LOCK_INITIALIZED) 
         fatal("Attempt to acquire an uninitialized lock!");
 
 #ifdef WIN32
@@ -45,9 +49,9 @@ void cas_lock_release(CasLock *lock) {
     lock->owner = 0;
 
 #ifdef WIN32
-    InterlockedExchange(&lock->lock, 0);
+    InterlockedExchange(&lock->lock, CAS_LOCK_UNLOCKED);
 #else
-    __sync_bool_compare_and_swap(&lock->lock, 1, 0);
+    __sync_bool_compare_and_swap(&lock->lock, CAS_LOCK_LOCKED, CAS_LOCK_UNLOCKED);
 #endif
 }
 
