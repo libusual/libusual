0526d431482ce044f7780284e44df59bd8a570ec
Beihao Zhou <beihaozhou@gmail.com>
2025-03-12 22:28:09 -0400

    fix naming from CAS lock to spin lock

--- test/test_common.c
+++ test/test_common.c
@@ -11,7 +11,7 @@ struct testgroup_t groups[] = {
 	{ "aatree/", aatree_tests },
 	{ "base/", base_tests },
 	{ "bits/", bits_tests },
-	{ "caslock/", caslock_tests },
+	{ "spinlock/", spinlock_tests },
 	{ "cbtree/", cbtree_tests },
 	{ "cfparser/", cfparser_tests },
 	{ "crypto/", crypto_tests },
