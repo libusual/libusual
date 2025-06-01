ce2050be5983ad0002efaa6fd7fff723fb6b29b7
Beihao Zhou <beihaozhou@gmail.com>
2025-06-01 09:55:53 -0400

    fix tid

--- usual/spinlock.h
+++ usual/spinlock.h
@@ -2,15 +2,44 @@
 #define _SPIN_LOCK_H_
 
 #ifdef WIN32
-    #include <windows.h>
-    #define GET_THREAD_ID() ((uintptr_t)GetCurrentThreadId())
-    #define MEMORY_BARRIER() MemoryBarrier()
+#   include <windows.h>
+#   define GET_THREAD_ID() ((uintptr_t)GetCurrentThreadId()) 
+#   define MEMORY_BARRIER() MemoryBarrier()
+
+#elif defined(__APPLE__)
+#   include <pthread.h>
+    static inline uintptr_t _darwin_tid(void)
+    {
+        uint64_t tid;
+        /* always succeeds for current thread */
+        (void)pthread_threadid_np(NULL, &tid);
+        return (uintptr_t)tid;
+    }
+#   define GET_THREAD_ID() _darwin_tid()
+#   define MEMORY_BARRIER() __sync_synchronize()
+
 #else
-    #include <usual/pthread.h>
-    #include <sched.h>
-    #define GET_THREAD_ID() ((uintptr_t)pthread_self())
-    #define MEMORY_BARRIER() __sync_synchronize()
-#endif
+#   include <sched.h>
+#   include <unistd.h>
+#   include <sys/types.h>
+#   include <sys/syscall.h>
+#   if defined(SYS_gettid)
+        static inline uintptr_t _linux_tid(void)
+        {
+            return (uintptr_t)syscall(SYS_gettid);
+        }
+#       define GET_THREAD_ID() _linux_tid()
+#   else        /* very old BSDs / exotic libc */
+#       include <pthread.h>
+        static inline uintptr_t _fallback_tid(void)
+        {
+            pthread_t self = pthread_self();
+            return (uintptr_t)(void *)&self;   /* unique for lifetime */
+        }
+#       define GET_THREAD_ID() _fallback_tid()
+#   endif
+#   define MEMORY_BARRIER() __sync_synchronize()
+#endif /* platform switch */
 
 typedef struct {
     volatile uintptr_t lock_word;  // 0 = unlocked, otherwise holds thread ID
