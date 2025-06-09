bb40d2c1d60307cd8580c1be5d38b6e4c08cf4c9
GuanqunYang193 <gyang193@bloomberg.net>
2025-06-09 00:33:44 -0400

    use HAVE_PTHREAD_THREADID_NP

--- usual/spinlock.h
+++ usual/spinlock.h
@@ -8,16 +8,16 @@
 #   define GET_THREAD_ID() ((uintptr_t)GetCurrentThreadId()) 
 #   define MEMORY_BARRIER() MemoryBarrier()
 
-#elif defined(__APPLE__)
+#elif HAVE_PTHREAD_THREADID_NP
 #   include <pthread.h>
-    static inline uintptr_t _darwin_tid(void)
+    static inline uintptr_t _pthread_tid(void)
     {
         uint64_t tid;
         /* always succeeds for current thread */
         (void)pthread_threadid_np(NULL, &tid);
         return (uintptr_t)tid;
     }
-#   define GET_THREAD_ID() _darwin_tid()
+#   define GET_THREAD_ID() _pthread_tid()
 #   define MEMORY_BARRIER() __sync_synchronize()
 
 #else
